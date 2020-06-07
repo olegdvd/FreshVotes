bcc960a1fd2bc027aad9556046470303c917e841
Oleg <oleg.dvd@gmail.com>
2020-06-07 18:17:59 +0300

    added registration page without writing to the database

--- src/main/java/com/freshvotes/web/LoginController.java
+++ src/main/java/com/freshvotes/web/LoginController.java
@@ -1,7 +1,10 @@
 package com.freshvotes.web;
 
+import com.freshvotes.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -11,7 +14,14 @@ public class LoginController {
     }
 
     @GetMapping("register")
-    public  String register(){
+    public String register(ModelMap model) {
+        model.put("user", new User());
         return "register";
+    }
+
+    @PostMapping("register")
+    public String registerPost(User user) {
+        System.out.println(user);
+        return "redirect:/register";
     }
 }
