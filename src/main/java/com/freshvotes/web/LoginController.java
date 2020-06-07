791d453224bd271824ca64f68c0c529c4745375d
Oleg <oleg.dvd@gmail.com>
2020-06-07 19:14:04 +0300

    added Create Account functionality

--- src/main/java/com/freshvotes/web/LoginController.java
+++ src/main/java/com/freshvotes/web/LoginController.java
@@ -1,6 +1,8 @@
 package com.freshvotes.web;
 
 import com.freshvotes.domain.User;
+import com.freshvotes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("login")
     public String login() {
         return "login2";
@@ -21,7 +26,7 @@ public class LoginController {
 
     @PostMapping("register")
     public String registerPost(User user) {
-        System.out.println(user);
-        return "redirect:/register";
+        userService.save(user);
+        return "redirect:/login";
     }
 }
